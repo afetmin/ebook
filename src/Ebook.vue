c0d3ce968e17a1b4cdf249af11365e93b0ce5cf7
afetmin <kingpenguins@163.com>
2020-05-22 10:08:31 +0800

    电子书翻页

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,5 +1,14 @@
 <template>
-    <div id="read"></div>
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +17,16 @@ const EBOOK_URL = '/static/人类简史.epub'
 export default {
   name: 'ebook',
   methods: {
+    prevPage() {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next()
+      }
+    },
     // 电子书的解析与渲染
     showEpub() {
       // 生成电子书
@@ -29,4 +48,27 @@ export default {
 
 <style lang='scss' scoped>
 @import 'assets/styles/global';
+.ebook {
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      .left {
+        flex: 0 0 px2rem(100);
+      }
+      .center {
+        flex: 1;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+      }
+    }
+  }
+}
 </style>
