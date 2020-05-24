5df0dfb5697856b11a8760ede6b8eabb953979fa
afetmin <kingpenguins@163.com>
2020-05-24 12:03:14 +0800

    目录页和进度条

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="ebook">
-    <title-bar :menuShow="menuShow"></title-bar>
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
     <div class="read-wrapper">
       <div id="read"></div>
       <div class="mask">
         <div class="left" @click="prevPage"></div>
-        <div class="center" @click="toggleTitleMenu"></div>
+        <div class="center" @click="toggleTitleAndMenu"></div>
         <div class="right" @click="nextPage"></div>
       </div>
     </div>
-    <menu-bar :menuShow="menuShow"
-    ref="menuBar"
-    @setFontSize="setFontSize"
-    :fontSizeList="fontSizeList"
-    :defaultFontSize="defaultFontSize"
-    ></menu-bar>
+    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"
+              :fontSizeList="fontSizeList"
+              :defaultFontSize="defaultFontSize"
+              @setFontSize="setFontSize"
+              :themeList="themeList"
+              :defaultTheme="defaultTheme"
+              @setTheme="setTheme"
+              :bookAvailable="bookAvailable"
+              @onProgressChange="onProgressChange"
+              :navigation="navigation"
+              @jumpTo="jumpTo"
+              ref="menuBar"></menu-bar>
   </div>
 </template>
 
@@ -22,12 +28,15 @@
 import TitleBar from '@/components/TitleBar'
 import MenuBar from '@/components/MenuBar'
 import Epub from 'epubjs'
-const EBOOK_URL = '/static/人类简史.epub'
+const DOWNLOAD_URL = '/static/人类简史.epub'
 export default {
-  name: 'ebook',
+  components: {
+    TitleBar,
+    MenuBar
+  },
   data() {
     return {
-      menuShow: false,
+      ifTitleAndMenuShow: false,
       fontSizeList: [
         { fontSize: 12 },
         { fontSize: 14 },
@@ -37,62 +46,146 @@ export default {
         { fontSize: 22 },
         { fontSize: 24 }
       ],
-      // 控制显示字体大小
-      defaultFontSize: 16
+      defaultFontSize: 16,
+      themeList: [
+        {
+          name: 'default',
+          style: {
+            body: {
+              'color': '#000', 'background': '#fff'
+            }
+          }
+        },
+        {
+          name: 'green',
+          style: {
+            body: {
+              'color': '#000', 'background': '#ceeaba'
+            }
+          }
+        },
+        {
+          name: 'night',
+          style: {
+            body: {
+              'color': '#fff', 'background': '#000'
+            }
+          }
+        },
+        {
+          name: 'gold',
+          style: {
+            body: {
+              'color': '#000', 'background': 'rgb(241, 236, 226)'
+            }
+          }
+        }
+      ],
+      defaultTheme: 0,
+      // 图书是否处于可用状态
+      bookAvailable: false,
+      navigation: {}
     }
   },
   methods: {
+    // 根据链接跳转到指定位置
+    jumpTo(href) {
+      this.rendition.display(href)
+      this.hideTitleAndMenu()
+    },
+    hideTitleAndMenu() {
+      // 隐藏标题栏和菜单栏
+      this.ifTitleAndMenuShow = false
+      // 隐藏菜单栏弹出的设置栏
+      this.$refs.menuBar.hideSetting()
+      // 隐藏目录
+      this.$refs.menuBar.hideContent()
+    },
+    // progress 进度条的数值（0-100）
+    onProgressChange(progress) {
+      const percentage = progress / 100
+      const location = percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0
+      this.rendition.display(location)
+    },
+    // 设置主题
+    setTheme(index) {
+      this.themes.select(this.themeList[index].name)
+      this.defaultTheme = index
+    },
+    // 注册主题
+    registerTheme() {
+      this.themeList.forEach(theme => {
+        this.themes.register(theme.name, theme.style)
+      })
+    },
+    // 设置字号大小
     setFontSize(fontSize) {
       this.defaultFontSize = fontSize
       if (this.themes) {
         this.themes.fontSize(fontSize + 'px')
       }
     },
-    toggleTitleMenu() {
-      this.menuShow = !this.menuShow
-      if (!this.menuShow) {
-        this.$refs.menuBar.hide()
+    // 切换标题和菜单的显示状态
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (!this.ifTitleAndMenuShow) {
+        this.$refs.menuBar.hideSetting()
       }
     },
+    // 上一页
     prevPage() {
       if (this.rendition) {
         this.rendition.prev()
       }
     },
+    // 下一页
     nextPage() {
       if (this.rendition) {
         this.rendition.next()
       }
     },
-    // 电子书的解析与渲染
+    // 电子书的解析和渲染
     showEpub() {
-      // 生成电子书
-      this.book = new Epub(EBOOK_URL)
-      // 生成Rendition
+      // 生成Book对象
+      this.book = new Epub(DOWNLOAD_URL)
+      // 通过Book.renderTo生成Rendition对象
       this.rendition = this.book.renderTo('read', {
         width: window.innerWidth,
-        height: window.innerHeight
+        height: window.innerHeight,
+        // 兼容iOS
+        method: 'default'
       })
-      // 通过Rendition.display渲染电子书
+      // 通过Rendtion.display渲染电子书
       this.rendition.display()
-      // 获取theme对象
+      // 获取Theme对象
       this.themes = this.rendition.themes
-      // 设置字体大小
+      // 设置默认字体
       this.setFontSize(this.defaultFontSize)
+      // 注册主题
+      this.registerTheme()
+      // 设置默认主题
+      this.setTheme(this.defaultTheme)
+      // Book对象的钩子函数ready
+      this.book.ready.then(() => {
+        this.navigation = this.book.navigation
+        // 生成Locations对象
+        return this.book.locations.generate()
+      }).then(() => {
+        // 保存locations对象
+        this.locations = this.book.locations
+        // 标记电子书为解析完毕状态
+        this.bookAvailable = true
+      })
     }
   },
   mounted() {
     this.showEpub()
-  },
-  components: {
-    TitleBar,
-    MenuBar
   }
 }
 </script>
 
 <style lang='scss' scoped>
-@import "assets/styles/global";
+@import 'assets/styles/global';
 .ebook {
   position: relative;
   .read-wrapper {
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,112 +1,172 @@
 <template>
   <div class="menu-bar">
     <transition name="slide-up">
-      <div class="menu-wrapper"
-        v-show="menuShow"
-        :class="{'hideBoxShadow': settingShow || !menuShow}">
+      <div class="menu-wrapper" :class="{'hide-box-shadow': ifSettingShow || !ifTitleAndMenuShow}" v-show="ifTitleAndMenuShow">
         <div class="icon-wrapper">
-          <span class="icon icon-menu"></span>
+          <span class="icon-menu icon" @click="showSetting(3)"></span>
         </div>
         <div class="icon-wrapper">
-          <span class="icon icon-progress"></span>
+          <span class="icon-progress icon" @click="showSetting(2)"></span>
         </div>
         <div class="icon-wrapper">
-          <span class="icon icon-bright"></span>
+          <span class="icon-bright icon" @click="showSetting(1)"></span>
         </div>
         <div class="icon-wrapper">
-          <span class="icon icon-a" @click="show">A</span>
+          <span class="icon-a icon" @click="showSetting(0)">A</span>
         </div>
       </div>
     </transition>
     <transition name="slide-up">
-    <div class="setting-wrapper" v-show="settingShow">
-      <div class="setting-font-size">
-        <div class="preview" :style="{fontSize:fontSizeList[0].fontSize + 'px'}">A</div>
-        <div class="select">
-          <div class="select-wrapper"
-          @click="setFontSize(item.fontSize)"
-          v-for="(item,index) in fontSizeList" :key="index">
-            <div class="line"></div>
-            <div class="point-wrapper">
-              <div class="point" v-show="defaultFontSize === item.fontSize" >
-                <div class="small-point"></div>
+      <div class="setting-wrapper" v-show="ifSettingShow">
+        <div class="setting-font-size" v-if="showTag === 0">
+          <div class="preview" :style="{fontSize: fontSizeList[0].fontSize + 'px'}">A</div>
+          <div class="select">
+            <div class="select-wrapper" v-for="(item, index) in fontSizeList" :key="index" @click="setFontSize(item.fontSize)">
+              <div class="line"></div>
+              <div class="point-wrapper">
+                <div class="point" v-show="defaultFontSize === item.fontSize">
+                  <div class="small-point"></div>
+                </div>
               </div>
+              <div class="line"></div>
             </div>
-            <div class="line"></div>
+          </div>
+          <div class="preview" :style="{fontSize: fontSizeList[fontSizeList.length - 1].fontSize + 'px'}">A</div>
+        </div>
+        <div class="setting-theme" v-else-if="showTag === 1">
+          <div class="setting-theme-item" v-for="(item, index) in themeList" :key="index" @click="setTheme(index)">
+            <div class="preview" :style="{background: item.style.body.background}" :class="{'no-border': item.style.body.background !== '#fff'}"></div>
+            <div class="text" :class="{'selected': index === defaultTheme}">{{item.name}}</div>
           </div>
         </div>
-        <div class="preview" :style="{fontSize:fontSizeList[fontSizeList.length-1].fontSize + 'px'}">A</div>
+        <div class="setting-progress" v-else-if="showTag === 2">
+          <div class="progress-wrapper">
+            <input class="progress" type="range"
+                                    max="100"
+                                    min="0"
+                                    step="1"
+                                    @change="onProgressChange($event.target.value)" @input="onProgressInput($event.target.value)"
+                                    :value="progress"
+                                    :disabled="!bookAvailable"
+                                    ref="progress">
+          </div>
+          <div class="text-wrapper">
+            <span>{{bookAvailable ? progress + '%' : '加载中...'}}</span>
+          </div>
+        </div>
       </div>
-    </div>
+    </transition>
+    <content-view :ifShowContent="ifShowContent"
+                    v-show="ifShowContent"
+                    :navigation="navigation"
+                    :bookAvailable="bookAvailable"
+                    @jumpTo="jumpTo"></content-view>
+    <transition name="fade">
+      <div class="content-mask"
+            v-show="ifShowContent"
+            @click="hideContent"></div>
     </transition>
   </div>
 </template>
 
 <script>
-
+import ContentView from '@/components/Content'
 export default {
-  name: 'menubar',
+  components: {
+    ContentView
+  },
   props: {
-    menuShow: {
+    ifTitleAndMenuShow: {
       type: Boolean,
       default: false
     },
-    fontSizeList: {
-      type: Array,
-      default() {
-        return []
-      }
+    fontSizeList: Array,
+    defaultFontSize: Number,
+    themeList: Array,
+    defaultTheme: Number,
+    bookAvailable: {
+      type: Boolean,
+      default: false
     },
-    defaultFontSize: Number
+    navigation: Object
   },
   data() {
     return {
-      settingShow: false
-    }
-  },
-  computed: {
-    getletterAwidth() {
-      return ((((window.innerWidth - 80) / 7) / 2) + 40) / 2
+      ifSettingShow: false,
+      showTag: 0,
+      progress: 0,
+      ifShowContent: false
     }
   },
   methods: {
+    // 隐藏目录
+    hideContent() {
+      this.ifShowContent = false
+    },
+    // 跳转方法，调用父组件方法
+    jumpTo(target) {
+      this.$emit('jumpTo', target)
+    },
+    // 拖动进度条时触发事件
+    onProgressInput(progress) {
+      this.progress = progress
+      this.$refs.progress.style.backgroundSize = `${this.progress}% 100%`
+    },
+    // 进度条松开后触发事件，根据进度条数值跳转到指定位置
+    onProgressChange(progress) {
+      this.$emit('onProgressChange', progress)
+    },
+    // 设置主题
+    setTheme(index) {
+      this.$emit('setTheme', index)
+    },
+    // 设置字号
     setFontSize(fontSize) {
       this.$emit('setFontSize', fontSize)
     },
-    show() {
-      this.settingShow = true
+    // 显示设置面板
+    showSetting(tag) {
+      this.showTag = tag
+      if (this.showTag === 3) {
+        this.ifSettingShow = false
+        this.ifShowContent = true
+      } else {
+        this.ifSettingShow = true
+      }
     },
-    hide() {
-      this.settingShow = false
+    // 显示设置面板
+    hideSetting() {
+      this.ifSettingShow = false
     }
   }
 }
+
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 @import '../assets/styles/global';
 .menu-bar {
   .menu-wrapper {
     position: absolute;
     bottom: 0;
     left: 0;
-    z-index: 101;
+    z-index: 102;
     display: flex;
     width: 100%;
     height: px2rem(48);
     background: white;
-    box-shadow: 0 px2rem(-8) px2rem(6) rgba(0, 0, 0, 0.15);
-    &.hideBoxShadow {
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    &.hide-box-shadow {
       box-shadow: none;
     }
     .icon-wrapper {
       flex: 1;
       @include center;
-      .icon-menu {
-        font-size: px2rem(24);
-      }
       .icon-progress {
-        font-size: px2rem(26);
+        font-size: px2rem(28);
+      }
+      .icon-bright {
+        font-size: px2rem(24);
       }
     }
   }
@@ -118,7 +178,7 @@ export default {
     width: 100%;
     height: px2rem(60);
     background: white;
-    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
     .setting-font-size {
       display: flex;
       height: 100%;
@@ -167,7 +227,7 @@ export default {
               border-radius: 50%;
               background: white;
               border: px2rem(1) solid #ccc;
-              box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, 0.15);
+              box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, .15);
               @include center;
               .small-point {
                 width: px2rem(5);
@@ -180,6 +240,84 @@ export default {
         }
       }
     }
+    .setting-theme {
+      height: 100%;
+      display: flex;
+      .setting-theme-item {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: px2rem(5);
+        box-sizing: border-box;
+        .preview {
+          flex: 1;
+          border: px2rem(1) solid #ccc;
+          box-sizing: border-box;
+          &.no-border {
+            border: none;
+          }
+        }
+        .text {
+          flex: 0 0 px2rem(20);
+          font-size: px2rem(14);
+          color: #ccc;
+          @include center;
+          &.selected {
+            color: #333;
+          }
+        }
+      }
+    }
+    .setting-progress {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        .progress-wrapper {
+          width: 100%;
+          height: 100%;
+          @include center;
+          padding: 0 px2rem(30);
+          box-sizing: border-box;
+          .progress {
+            width: 100%;
+            -webkit-appearance: none;
+            height: px2rem(2);
+            background: -webkit-linear-gradient(#999, #999) no-repeat, #ddd;
+            background-size: 0 100%;
+            &:focus {
+              outline: none;
+            }
+            &::-webkit-slider-thumb {
+              -webkit-appearance: none;
+              height: px2rem(20);
+              width: px2rem(20);
+              border-radius: 50%;
+              background: white;
+              box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .15);
+              border: px2rem(1) solid #ddd;
+            }
+          }
+        }
+        .text-wrapper {
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          color: #333;
+          font-size: px2rem(12);
+          text-align: center;
+        }
+      }
+  }
+  .content-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background: rgba(51, 51, 51, .8);
   }
 }
 </style>
